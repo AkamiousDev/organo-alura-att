23046e5ce3e7f8e07d8f29f9a2cd66616e4cd8db
Caio Fernandes <caio.fsrodriguesp@gmail.com>
2023-02-01 15:11:24 -0300

    atualizacao

---
+++ src/componentes/banner/banner.js
@@ -0,0 +1,10 @@
+import './banner.css';
+
+export const Banner = () => {
+    return (
+        <header>
+            <img src="/imagens/banner.png" alt="O banner principal do Organo"/>
+        </header>
+    )
+}
+
